c2d6d61ed67a0c3f623480b7e927c57b7e915cec
Lamine005 <133173949+Lamine005@users.noreply.github.com>
2023-11-29 03:57:50 -0500

    index js

--- index.js
+++ index.js
@@ -22,11 +22,24 @@ const CardArray = [
 
 CardArray.sort(()=> 0.5 - Math.random);
 const gridDisplay = document.querySelector('#grid');
+const cardChosen = [];
+
 function createBoard(){
     for(let i=0; i<CardArray.length;i++){
         const card = document.createElement('img');
         card.setAttribute('src','images/blank.png');
+        card.addEventListener('click',flipcard())
         card.setAttribute('data-img',i)
         gridDisplay.appendChild(card)
     }
+}
+
+createBoard();
+
+function flipcard(){
+    const cardId = this.getAttribute('data-id');
+    
+    cardChosen.push(CardArray[cardId].name)
+    this.setAttribute('src',CardArray[cardId].img)
+
 }
